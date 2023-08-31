3731ee477158f62ba195f8cddff2e46d64956576
Hubert Cadieux <hubert.cadieux.1@ulaval.ca>
2023-08-31 14:29:53 -0400

    liste graphs a produire

--- Code/descriptif_bivar.R
+++ Code/descriptif_bivar.R
@@ -5,3 +5,23 @@ library(tidyverse)
 Data <- readRDS("_SharedFolder_Guide_mve/data/clean.rds")
 
 
+## guide_use_format
+
+## comparer guide_use_format_why selon papier ou web
+
+## comparer guide_paperfrequency et guide_web_freq
+
+## comparer guide_paper_prop et guide_web_prop
+
+## comparer satisfaction_papier et satisfaction_papier
+
+## guide_change pour les en ligne
+
+## guide_papier_prac VS guide_web_prac
+
+## guide_papier_dis VS guide_web_dis
+
+## guide_format_change
+
+
+
