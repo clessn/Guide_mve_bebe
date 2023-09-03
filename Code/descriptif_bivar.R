9675524cf4590e1dc4ac06bfd09b05b8e7f71eb0
Hubert Cadieux <hubert.cadieux.1@ulaval.ca>
2023-09-03 12:45:22 -0400

    graph format utilise

--- Code/descriptif_bivar.R
+++ Code/descriptif_bivar.R
@@ -23,8 +23,9 @@ Data %>%
   scale_x_discrete(labels = c("0" = "Non",
                               "1" = "Oui",
                               "NA" = "N'a pas répondu")) +
+  scale_y_continuous(limits = c(0,950)) +
   clessnverse::theme_clean_light(base_size = 15) +
-  geom_text(aes(y = n + 10, label = paste0("n = ", n))) +
+  geom_text(aes(y = n + 15, label = paste0("n = ", n))) +
   ylab("Nombre de répondants") +
   xlab("") +
   labs(caption = "Les 926 répondants du sondage sont représentés sur ce graphique.",
@@ -56,8 +57,9 @@ ggplot(Graph, aes(x = guide_use_1, y = n)) +
   scale_x_discrete(labels = c("0" = "Non",
                               "1" = "Oui",
                               "NA" = "N'a pas répondu")) +
+  scale_y_continuous(limits = c(0,950)) +
   clessnverse::theme_clean_light(base_size = 15) +
-  geom_text(aes(y = n + 10, label = paste0("n = ", n))) +
+  geom_text(aes(y = n + 15, label = paste0("n = ", n))) +
   ylab("Nombre de répondants") +
   xlab("") +
   labs(caption = paste0("Les ", sum(Graph$n), " répondants qui connaissent le guide MVE sont représentés sur ce graphique."),
@@ -79,27 +81,41 @@ sum(table[1:5,2,2])
 Graph <- Data %>% 
   filter(guide_use_1 == 1) %>% 
   group_by(guide_use_format) %>% 
-  summarise(n = n()) %>% 
-  
-  ggplot(Graph, aes(x = guide_use_1, y = n)) +
+  summarise(n = n()) %>%
+  drop_na(guide_use_format) %>% 
+  mutate(guide_use_format = factor(guide_use_format,
+                                   levels = c("0", "0.25",
+                                              "0.5", "0.75", "1"),
+                                   labels = c("Uniquement papier",
+                                              "Principalement papier",
+                                              "Deux formats également",
+                                              "Principalement web",
+                                              "Uniquement web")))
+
+ggplot(Graph, aes(x = guide_use_format, y = n)) +
   geom_bar(stat = "identity",
-           aes(fill = guide_use_1),
+           aes(fill = guide_use_format),
            show.legend = F,
            color = NA) +
-  scale_fill_manual(values = c("0" = "#FF6B6B",
-                               "1" = "#95E08E",
-                               "NA" = "grey")) +
-  scale_x_discrete(labels = c("0" = "Non",
-                              "1" = "Oui",
-                              "NA" = "N'a pas répondu")) +
+  scale_fill_manual(
+    values = c(
+      "Uniquement papier" = "#FFE4B5",
+      "Principalement papier" = "#D2B48C",
+      "Les deux également" = "#808080",
+      "Principalement web" = "#6699CC",
+      "Uniquement web" = "#333399"
+    )
+  ) +
   clessnverse::theme_clean_light(base_size = 15) +
   geom_text(aes(y = n + 10, label = paste0("n = ", n))) +
   ylab("Nombre de répondants") +
   xlab("") +
-  labs(caption = paste0("Les ", sum(Graph$n), " répondants qui connaissent le guide MVE sont représentés sur ce graphique."),
-       title = "Avez-vous consulté le guide MVE?")
+  labs(caption = "715 des 716 répondants qui ont consulté le guide MVE sont représentés sur ce graphique.\nUn répondant n'a pas répondu à cette question.",
+       title = "Format utilisé pour consulter le guide MVE") +
+  theme(axis.text.x = element_text(angle = 15))
 
-ggsave("_SharedFolder_Guide_mve/graphs/2guide_consulte.png",
+### Un répondant n'a pas répondu
+ggsave("_SharedFolder_Guide_mve/graphs/3guide_format.png",
        width = 10, height = 8)
 
 
